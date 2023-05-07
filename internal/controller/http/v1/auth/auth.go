5398bab660fd4b19a634ce4d6cf1011773031a1b
Lev Erminson <erminson@gmail.com>
2023-05-07 14:57:33 +0300

    Add errorResponse method

--- internal/controller/http/v1/auth/auth.go
+++ internal/controller/http/v1/auth/auth.go
@@ -3,11 +3,18 @@ package auth_handler
 import (
 	"context"
 	"encoding/json"
-	"github.com/erminson/auth-var/internal/entity"
+	"errors"
+	"fmt"
 	"github.com/erminson/auth-var/internal/usecase"
 	"github.com/julienschmidt/httprouter"
 	"io"
 	"net/http"
+	"regexp"
+)
+
+var (
+	ErrPhoneNumber = errors.New("invalid phone number")
+	ErrConfirmCode = errors.New("invalid confirm code")
 )
 
 type AuthPhoneRequest struct {
@@ -35,7 +42,7 @@ type JsonErrorResponse struct {
 }
 
 type ApiError struct {
-	Status int16  `json:"status"`
+	Status int    `json:"status"`
 	Title  string `json:"title"`
 }
 
@@ -52,34 +59,46 @@ func New(confirmation *usecase.Confirmation) *AuthHandler {
 func (h *AuthHandler) GenerateConfirmationCode(ctx context.Context) httprouter.Handle {
 	fn := func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		data, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			response := JsonErrorResponse{Error: &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}}
-			if err = json.NewEncoder(w).Encode(response); err != nil {
-				panic(err)
-			}
+			apiError := &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}
+			h.errorResponse(w, apiError)
 			return
 		}
 
 		var authPhone AuthPhoneRequest
 		err = json.Unmarshal(data, &authPhone)
 		if err != nil {
-			// logging
+			apiError := &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}
+			h.errorResponse(w, apiError)
+			return
+		}
 
-			w.WriteHeader(http.StatusBadRequest)
-			response := JsonErrorResponse{Error: &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}}
-			if err = json.NewEncoder(w).Encode(response); err != nil {
-				panic(err)
+		err = h.validateAuthPhoneRequest(&authPhone)
+		if err != nil {
+			apiError := &ApiError{
+				Status: http.StatusBadRequest,
+				Title:  fmt.Sprintf("Bad Request. Error: %s", err.Error()),
 			}
+			h.errorResponse(w, apiError)
+			return
+		}
+
+		msg, err := h.u.GenerateConfirmationCode(ctx, authPhone.Phone)
+		if err != nil {
+			apiError := &ApiError{
+				Status: http.StatusBadRequest,
+				Title:  fmt.Sprintf("Bad Request. Error: %s", err.Error()),
+			}
+			h.errorResponse(w, apiError)
 			return
 		}
 
 		m := MessageResponse{
-			Text:   "SMS",
-			Digit:  4,
-			Source: string(entity.Sms),
+			Text:   msg.Text,
+			Digit:  msg.Digit,
+			Source: string(msg.Source),
 		}
-
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(m); err != nil {
@@ -93,33 +112,45 @@ func (h *AuthHandler) GenerateConfirmationCode(ctx context.Context) httprouter.H
 func (h *AuthHandler) ConfirmPhoneNumber(ctx context.Context) httprouter.Handle {
 	fn := func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		data, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			response := JsonErrorResponse{Error: &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}}
-			if err = json.NewEncoder(w).Encode(response); err != nil {
-				panic(err)
-			}
+			apiError := &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}
+			h.errorResponse(w, apiError)
 			return
 		}
 
 		var phoneConfirm PhoneConfirmRequest
 		err = json.Unmarshal(data, &phoneConfirm)
 		if err != nil {
-			// logging
+			apiError := &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}
+			h.errorResponse(w, apiError)
+			return
+		}
 
-			w.WriteHeader(http.StatusBadRequest)
-			response := JsonErrorResponse{Error: &ApiError{Status: http.StatusBadRequest, Title: "Bad Request"}}
-			if err = json.NewEncoder(w).Encode(response); err != nil {
-				panic(err)
+		err = h.validatePhoneConfirmRequest(&phoneConfirm, 4)
+		if err != nil {
+			apiError := &ApiError{
+				Status: http.StatusBadRequest,
+				Title:  fmt.Sprintf("Bad Request. Error: %s", err.Error()),
 			}
+			h.errorResponse(w, apiError)
+			return
+		}
+
+		t, err := h.u.ConfirmPhoneNumber(ctx, phoneConfirm.Phone, phoneConfirm.Code)
+		if err != nil {
+			apiError := &ApiError{
+				Status: http.StatusBadRequest,
+				Title:  fmt.Sprintf("Bad Request. Error: %s", err.Error()),
+			}
+			h.errorResponse(w, apiError)
 			return
 		}
 
 		m := TokensResponse{
-			Access:  "access_token",
-			Refresh: "refresh_token",
+			Access:  t.Access,
+			Refresh: t.Refresh,
 		}
-
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
 		if err := json.NewEncoder(w).Encode(m); err != nil {
@@ -128,4 +159,48 @@ func (h *AuthHandler) ConfirmPhoneNumber(ctx context.Context) httprouter.Handle 
 	}
 
 	return fn
+}
+
+func (h *AuthHandler) errorResponse(w http.ResponseWriter, apiError *ApiError) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(apiError.Status)
+
+	response := JsonErrorResponse{Error: apiError}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		panic(err)
+	}
+}
+
+func (h *AuthHandler) validateAuthPhoneRequest(in *AuthPhoneRequest) error {
+	if len(in.Phone) == 0 {
+		return ErrPhoneNumber
+	}
+
+	re := regexp.MustCompile(`^\d+$`)
+	if !re.MatchString(in.Phone) {
+		return ErrPhoneNumber
+	}
+
+	return nil
+}
+
+func (h *AuthHandler) validatePhoneConfirmRequest(in *PhoneConfirmRequest, d int) error {
+	if len(in.Phone) == 0 {
+		return ErrPhoneNumber
+	}
+
+	re := regexp.MustCompile(`^\d+$`)
+	if !re.MatchString(in.Phone) {
+		return ErrPhoneNumber
+	}
+
+	if len(in.Code) != d {
+		return ErrConfirmCode
+	}
+
+	if !re.MatchString(in.Code) {
+		return ErrConfirmCode
+	}
+
+	return nil
 }
